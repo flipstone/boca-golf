997e92bb56df129a109a8632798cd62b12b54f95
David Vollbracht <david.vollbracht@gmail.com>
2010-12-24 23:31:43 -0500

    Run gists at safe level 4 to prevent user evilness

--- lib/boca_golf.rb
+++ lib/boca_golf.rb
@@ -2,11 +2,27 @@ class BocaGolf
   def run(args, stdout, stderr)
     gist_url, *rspec_args = args
     gist = Net::HTTP.get URI.parse(gist_url + ".txt")
-    gist_module = Module.new
-    gist_module.module_eval gist
+
+    gist_module = lambda do
+      $SAFE = 4
+      Module.new.tap {|m| m.module_eval gist}
+    end.call
+
+    safe_proxy = Module.new
+    gist_module.instance_methods.each do |method|
+      safe_proxy.module_eval %{
+        def #{method}(*args, &block)
+          lambda do
+            $SAFE = 4
+            super
+          end.call
+        end
+      }
+    end
 
     RSpec.configure do |config|
       config.include gist_module
+      config.include safe_proxy
     end
 
     RSpec::Core::Runner.run rspec_args, stderr, stdout
--- spec/boca_golf_spec.rb
+++ spec/boca_golf_spec.rb
@@ -5,48 +5,65 @@ require 'boca_golf'
 
 FakeWeb.allow_net_connect = false
 
-def sandboxed(&block)
-  begin
-    @orig_config = RSpec.configuration
-    @orig_world  = RSpec.world
+module RSpecTestHelper
+  def sandboxed(&block)
+    begin
+      @orig_config = RSpec.configuration
+      @orig_world  = RSpec.world
 
-    new_config = RSpec::Core::Configuration.new
-    new_world  = RSpec::Core::World.new(new_config)
+      new_config = RSpec::Core::Configuration.new
+      new_world  = RSpec::Core::World.new(new_config)
 
-    RSpec.instance_variable_set(:@configuration, new_config)
-    RSpec.instance_variable_set(:@world, new_world)
-    yield
-  ensure
-    RSpec.instance_variable_set(:@configuration, @orig_config)
-    RSpec.instance_variable_set(:@world, @orig_world)
+      RSpec.instance_variable_set(:@configuration, new_config)
+      RSpec.instance_variable_set(:@world, new_world)
+      yield
+    ensure
+      RSpec.instance_variable_set(:@configuration, @orig_config)
+      RSpec.instance_variable_set(:@world, @orig_world)
+    end
   end
+end
+
+RSpec.configure do |c|
+  c.include RSpecTestHelper
 end
 
 describe BocaGolf do
   it "returns true if all specs pass" do
-    FakeWeb.register_uri :get, "http://gist.github.com/746166.txt", body: "def reverse(a) a.reverse; end"
-    stdout, stderr = StringIO.new, StringIO.new
-    sandboxed do
-      BocaGolf.new.run(["http://gist.github.com/746166", "spec/infrastructure/reverse_specs/spec.rb"], stdout, stderr)
-    end.should be_true
+    run_specs_on_gist("def reverse(a) a.reverse; end").should be_true
   end
 
   it "returns false if some specs fail" do
-    FakeWeb.register_uri :get, "http://gist.github.com/746166.txt", body: "def reverse(a) a; end"
-    stdout, stderr = StringIO.new, StringIO.new
-    sandboxed do
-      BocaGolf.new.run(["http://gist.github.com/746166", "spec/infrastructure/reverse_specs/spec.rb"], stdout, stderr)
-    end.should be_false
+    run_specs_on_gist("def reverse(a) a; end").should be_false
   end
 
   it "doesn't make methods available everywhere" do
-    FakeWeb.register_uri :get, "http://gist.github.com/746166.txt", body: "def foobar(a) a; end"
+    run_specs_on_gist "def foobar(a) a; end"
+    -> { foobar '1' }.should raise_error NoMethodError
+  end
+
+  it "evals code at safe level 4" do
+    lambda do
+      run_specs_on_gist("def reverse(a) a.reverse; end; class ::Object; def foo() end; end")
+    end.should raise_error(SecurityError)
+  end
+
+  it "calls methods at safe level 4" do
+    run_specs_on_gist("def reverse(a) ::Object.class_eval { def foo() end }; a.reverse; end").should be_false
+
+    lambda do
+      Object.new.foo
+    end.should raise_error(NoMethodError)
+  end
+
+  def run_specs_on_gist(gist)
+    gist.taint
+    gist.untrust
+
+    FakeWeb.register_uri :get, "http://gist.github.com/746166.txt", body: gist
     stdout, stderr = StringIO.new, StringIO.new
     sandboxed do
       BocaGolf.new.run(["http://gist.github.com/746166", "spec/infrastructure/reverse_specs/spec.rb"], stdout, stderr)
     end
-    lambda do
-      foobar '1'
-    end.should raise_error NoMethodError
   end
 end
